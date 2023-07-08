b6902240af5e9bc72dfae3b634312abcb489ca52
StaTov <tovchfamily@gmail.com>
2023-07-08 16:18:10 +0300

    add admin logout

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -3,24 +3,29 @@ import { toLoginAdmin } from '../utils/checkType';
 import { Response, Request, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
 
+//authentication
 export const loginAdmin = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = toLoginAdmin(req.body);
-      
+
         const existAdmin = await getAdminByEmail(email).select('+password');
         if (!existAdmin) {
-            res.status(404).send({error: 'wrong email'});
+            res.status(404).send({ error: 'wrong email' });
             return;
         }
         const existPWD = await bcrypt.compare(password, existAdmin.password);
         if (!existPWD) {
-            res.status(404).send({error: 'wrong password'});
+            res.status(404).send({ error: 'wrong password' });
             return;
         }
         existAdmin.token = await bcrypt.hash(existAdmin._id.toString(), 10);
         await existAdmin.save();
 
-        res.cookie('MUSK-AUTH', existAdmin.token, { domain: 'localhost', path: '/' } );
+        res.cookie('MUSK-AUTH', existAdmin.token, {
+            httpOnly: true,
+            domain: 'localhost',
+            path: '/'
+        });
         res.status(200).json({ admin: true });
 
     } catch (err) {
@@ -28,7 +33,20 @@ export const loginAdmin = async (req: Request, res: Response, next: NextFunction
     }
 };
 
-
+export const logOutAdmin = (_req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.cookie('MUSK-AUTH', 'none', {
+            expires: new Date(Date.now() + 5 * 1000),
+            httpOnly: true,
+            domain: 'localhost',
+            path: '/'
+        });
+        res.status(200).send('admin logout successully');
+    } catch (err) {
+        next(err);
+    }
+};
+//create new admin
 export const regAdmin = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = toLoginAdmin(req.body);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import express, { RequestHandler } from 'express';
-import { loginAdmin, regAdmin } from '../controllers/admin';
+import { logOutAdmin, loginAdmin, regAdmin } from '../controllers/admin';
+
 
 
 const router = express.Router();
 
 router.post('/admin/login', loginAdmin as RequestHandler);
+router.post('/admin/logout', logOutAdmin as RequestHandler);
 router.post('/admin/reg', regAdmin as RequestHandler);
 
 
